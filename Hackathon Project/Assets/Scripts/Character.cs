64c536d6d9c74f0420d535072599e8a1a7b67ada
Kyle DiMaggio <kydimaggio@ctemc.org>
2020-05-16 13:09:17 -0400

    created waypoint system

--- Hackathon Project/Assets/Scripts/Character.cs
+++ Hackathon Project/Assets/Scripts/Character.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float speed = 1;
+    private List<GameObject> waypoints = new List<GameObject>();
+    private GameObject waypointPrefab;
+
+    private void Awake()
     {
-        
+        waypointPrefab = (GameObject)Resources.Load("Prefabs/Waypoint");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            if (waypoints.Count == 0)
+            {
+                waypoints.Add(Instantiate(waypointPrefab, transform));
+            }
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitInfo) && Vector3.Distance(waypoints[waypoints.Count - 1].transform.position, hitInfo.point) > .25f)
+            {
+                waypoints.Add(Instantiate(waypointPrefab, hitInfo.point, Quaternion.identity));
+            }
+        }
     }
 }
