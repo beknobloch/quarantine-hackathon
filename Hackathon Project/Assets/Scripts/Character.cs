484746e75d87966d4ba9992afd83655a1d71ca2e
YoYoBoBo <46692722+YoYoBoBo@users.noreply.github.com>
2020-05-17 01:53:56 -0400

    Handsanitizer (and related things)
    c# code only

--- Hackathon Project/Assets/Scripts/Character.cs
+++ Hackathon Project/Assets/Scripts/Character.cs
@@ -12,14 +12,27 @@ public class Character : MonoBehaviour
     private bool drawingLine = false;
     private Vector3 moveVector = Vector3.zero;
 
+    private int hSTimer = 0;
+
+    private int tPTimer = 0;
+
+    private bool handSanitized = false;
+
+    private bool toiletPapered = false;
+
     [SerializeField]
     private string type;
     [SerializeField]
     private string color;
 
+    SpriteRenderer rend;
 
     private void Awake()
     {
+        
+        rend = GetComponent<SpriteRenderer>();
+        color = "red";
+        Debug.Log(rend.color);
         waypointPrefab = (GameObject)Resources.Load("Prefabs/Waypoint");
         rb = gameObject.GetComponent<Rigidbody>();
 
@@ -33,6 +46,7 @@ public class Character : MonoBehaviour
 	void Update()
 	{
 
+        
 		//  Creates waypoints.
 		if (Input.GetMouseButton(0))
 		{
@@ -59,6 +73,46 @@ public class Character : MonoBehaviour
 		else if (drawingLine) {
 			drawingLine = false;
 		}
+
+
+        //timer conditions
+        //handsanitizer
+        if (handSanitized){
+            hSTimer ++;
+
+            if (hSTimer >= 600){
+                Debug.Log("Yeet");
+                //set back to default (ADJUST VALUES)
+                if (type == "masked"){
+                    radius = 1f;
+                }
+                else {
+                    radius = 0;
+                }
+                 
+                handSanitized = false;
+                rend.color = Color.red;
+            }
+        }
+
+        //toilet paper
+        if (toiletPapered){
+            tPTimer ++;
+
+            if (tPTimer >= 600){
+
+                if (type == "masked"){
+                    speed = 25;
+                }
+                else {
+                    speed = 32;
+                }
+
+
+                toiletPapered = false;
+                rend.color = Color.red;
+            }
+        }
 	}
         //  Moves the player.
     void FixedUpdate()
@@ -83,16 +137,42 @@ public class Character : MonoBehaviour
 
     void OnTriggerEnter(Collider collision)
     {
+        Debug.Log("collide!");
         if(collision.gameObject.CompareTag("Flag") && collision.gameObject.GetComponent<Flag>().getColor().Equals(color)){
+            Debug.Log("1");
             gameObject.SetActive(false);
         }
         else if(collision.gameObject.tag == "Character"){
+            Debug.Log("2");
             //Lose level
             GameObject.Find("LosePanel").SetActive(true);
         }
+        else if(collision.gameObject.CompareTag("Hand Sanitizer"))
+        {
+            //Decrease radius for set amount of time
+            Debug.Log("3");
+            Destroy(collision.gameObject);
+            radius = 1; //change value as needed
+            hSTimer = 0;
+            handSanitized = true;
+            rend.color = Color.green; 
+            Debug.Log("Hi!");
+        }
+        else if(collision.gameObject.CompareTag("Toilet Paper"))
+        {
+            //increase speed for set amount of time
+            Debug.Log("4");
+            Destroy(collision.gameObject);
+            speed = speed+speed; //change value as needed
+            tPTimer = 0;
+            toiletPapered = true;
+            rend.material.color = Color.black;
+        }
         else
 		{
+            Debug.Log("5");
 			moveVector = rb.velocity;
 		}
     }
-}
+    }
+
