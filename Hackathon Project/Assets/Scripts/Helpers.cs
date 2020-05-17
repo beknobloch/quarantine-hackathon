1a1549ce564e64ac8507160a830e9de78f58a408
YoYoBoBo <46692722+YoYoBoBo@users.noreply.github.com>
2020-05-17 13:50:12 -0400

    fixed untracked files

--- Hackathon Project/Assets/Scripts/Helpers.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public static class Helpers
-{
-    public static Vector3 Vector3To2(Vector3 vector)
-    {
-        return new Vector3(vector.x, vector.y);
-    }
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# quarantine-hackathon
-Our project submission for the 2020 hths.hacks() hackathon.
-System.out.println("helloworld");
